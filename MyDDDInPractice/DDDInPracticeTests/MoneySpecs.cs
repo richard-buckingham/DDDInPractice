1f2c30cacf324a7486d09ea3c55d1020ac8ef46e
Richard Buckingham <richard.buckingham@hammertech.com.au>
2021-06-20 19:01:45 +1000

    add initial unit test

---
+++ MyDDDInPractice/DDDInPracticeTests/MoneySpecs.cs
@@ -0,0 +1,26 @@
+using DddInPractice.Logic;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DDDInPracticeTests
+{
+    public class MoneySpecs
+    {
+        [Fact]
+        public void Sum_of_two_moneys_produces_correct_result()
+        {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            Money sum = money1 + money2;
+
+            sum.OneCentCount.Should().Be(2);
+            sum.TenCentCount.Should().Be(4);
+            sum.QuarterCount.Should().Be(6);
+            sum.OneDollarCount.Should().Be(8);
+            sum.FiveDollarCount.Should().Be(10);
+            sum.TwentyDollarCount.Should().Be(12);
+        }
+    }
+}
